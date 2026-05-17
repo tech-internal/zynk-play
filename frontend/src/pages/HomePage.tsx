6c88c2e28416dde2b22f92e9d460658a93766751
KartikSuperstar <kartikey@superstarsai.com>
2026-05-17 21:31:57 +0530

    fix: remove invisible character from HomePage.tsx import statement

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useI18n, usePageTitle } from '../i18n';
 import './HomePage.css';
